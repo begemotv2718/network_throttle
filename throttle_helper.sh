58c309f806b3141377fca460953a050f599540cf
Viktor Bagin <viktor.bagin@promwad.com>
2019-04-09 11:36:29 +0300

    Replace legacy "route" command to "ip route"

--- throttle_helper.sh
+++ throttle_helper.sh
@@ -3,7 +3,7 @@
 setup(){
 ip link set dev ${VETH_NAME}_cont up
 ip addr add ${ADDR_PREFIX}2/24 dev ${VETH_NAME}_cont
-route add default gateway ${ADDR_PREFIX}1
+ip route add default via ${ADDR_PREFIX}1 dev ${VETH_NAME}_cont
 }
 
 cleanup(){
